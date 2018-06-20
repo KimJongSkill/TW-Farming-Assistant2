1ad3e92744385bfbf77aa95976ea8abeee99ae35
Antros Economides <antroseconomides@hotmail.com>
2018-06-20 21:12:41 +0300

    Refactor farm logic

--- js/farm.js
+++ js/farm.js
@@ -1,98 +1,99 @@
-var IntervalID;
+// Obtain list of villages and skip first two elements (they do not represent villages)
+const VillageList = Array.from(
+	document.getElementById("plunder_list").firstElementChild.children
+).splice(2);
 
-var Profiles = {};
+const Profiles = {
+	A: {
+		Value: 0,
+		Icon: ".farm_icon_a"
+	},
 
-Profiles.A = {
-	Value: 0,
-	Icon: ".farm_icon_a"
-}
+	B: {
+		Value: 1,
+		Icon: ".farm_icon_b"
+	}
+};
 
-Profiles.B = {
-	Value: 1,
-	Icon: ".farm_icon_b"
-}
-
-chrome.storage.sync.get(["Profile", "Delay"], function(Items)
-{
+chrome.storage.sync.get(["Profile", "Delay"], Items => {
 	/*
 	*	If an item is not set or an error occured,
 	*	the Storage API returns an undefined value.
 	*	In that case use the default value and alert
 	*	the user.
 	*/
-	if (Items.Profile == null || Items.Delay == null)
-	{
+	if (Items.Profile == null || Items.Delay == null) {
 		window.alert("Unable to retrieve settings.\nDefault values will be used.");
 		Items.Profile = "Auto"
 		Items.Delay = 225;
 	}
-	
-	if (Items.Profile == "Auto")
-	{
-		var Tables = document.querySelectorAll("#content_value > div.vis > div > form tbody");
-		var CapacityA = Tables[Profiles.A.Value].lastElementChild.lastElementChild.innerHTML;
-		var CapacityB = Tables[Profiles.B.Value].lastElementChild.lastElementChild.innerHTML;
-		
-		if (CapacityA > CapacityB)
-			IntervalID = window.setInterval(ClickAuto, Items.Delay, Profiles.A, Profiles.B);
-		else
-			IntervalID = window.setInterval(ClickAuto, Items.Delay, Profiles.B, Profiles.A);
+
+	if (Items.Profile == "Auto") {
+		const Tables = document.querySelectorAll("#content_value > div.vis > div > form tbody");
+		const CapacityA = Tables[Profiles.A.Value].lastElementChild.lastElementChild.innerHTML;
+		const CapacityB = Tables[Profiles.B.Value].lastElementChild.lastElementChild.innerHTML;
+
+		if (CapacityA > CapacityB) {
+			ClickAuto(Items.Delay, Profiles.A, Profiles.B);
+		}
+		else {
+			ClickAuto(Items.Delay, Profiles.B, Profiles.A);
+		}
 	}
-	else
-	{
-		IntervalID = window.setInterval(ClickSelected, Items.Delay, Profiles[Items.Profile]);
+	else {
+		ClickSelected(Items.Delay, Profiles[Items.Profile]);
 	}
 });
 
-var VillageList = document.getElementById("plunder_list").firstElementChild.children;
+function Sleep(ms) {
+	return new Promise(resolve => {
+		setTimeout(resolve, ms);
+	});
+}
 
-function GetUnitsHome()
-{
-	var UnitsList = document.getElementsByClassName("unit-item");
-	var Output = {};
-	
-	for (var i = 0; i < UnitsList.length; ++i)
-	{
-		Output[UnitsList[i].id] = window.parseInt(UnitsList[i].innerHTML);
+function GetUnitsHome() {
+	const UnitsList = document.getElementsByClassName("unit-item");
+	let Output = new Map();
+
+	for (const Unit of UnitsList) {
+		Output.set(Unit.id, window.parseInt(Unit.innerHTML));
 	}
-	
+
 	return Output;
 }
 
-function HasSufficientUnits(Profile)
-{
-	var UnitsHome = GetUnitsHome();
-	
-	for (var Unit in UnitsHome)
-	{
-		if (window.parseInt(document.querySelectorAll("[name=" + Unit + "]")
-			.item(Profile.Value).getAttribute("value")) > UnitsHome[Unit])
+function HasSufficientUnits(Profile) {
+	const UnitsHome = GetUnitsHome();
+
+	for (const [Unit, Count] of UnitsHome.entries()) {
+		if (window.parseInt(document.querySelectorAll(`[name=${Unit}]`)
+			.item(Profile.Value).getAttribute("value")) > Count) {
 			return false;
+		}
 	}
-	
+
 	return true;
 }
 
+async function ClickSelected(Interval, Profile) {
+	for (const Village of VillageList) {
+		if (!HasSufficientUnits(Profile)) break;
 
-function ClickSelected(Profile)
-{
-	if (VillageList.length > 1 && HasSufficientUnits(Profile))
-		VillageList[1].querySelector(Profile.Icon).click();
-	else
-		window.clearInterval(IntervalID);
+		Village.querySelector(Profile.Icon).click();
+		await Sleep(Interval);
+	}
 }
 
-function ClickAuto(Max, Min)
-{
-	if (VillageList.length > 1)
-	{
-		if (VillageList[1].getElementsByTagName("img")[2].src.endsWith("1.png") && HasSufficientUnits(Max))
-			VillageList[1].querySelector(Max.Icon).click();
-		else if (HasSufficientUnits(Min))
-			VillageList[1].querySelector(Min.Icon).click();
-		else
-			window.clearInterval(IntervalID);
+async function ClickAuto(Interval, Max, Min) {
+	for (const Village of VillageList) {
+		if (Village.getElementsByTagName("img")[2].src.endsWith("1.png") && HasSufficientUnits(Max)) {
+			Village.querySelector(Max.Icon).click();
+		} else if (HasSufficientUnits(Min)) {
+			Village.querySelector(Min.Icon).click();
+		} else {
+			break;
+		}
+
+		await Sleep(Interval);
 	}
-	else
-		window.clearInterval(IntervalID);
 }
